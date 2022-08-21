b2981432ab1087fdda195fea916fcaeca40600fc
bodeddy <rob.cutillo@gmail.com>
2022-08-21 18:17:33 +0100

    Add comments to explain what each line does
    As instructed in the book. This helped me to think about the code
    I had written.

--- 03-numbers-and-math/ex3.py
+++ 03-numbers-and-math/ex3.py
@@ -1,36 +1,54 @@
 """
 Math symbols for python:
-+ plus
-- minus
-/ slash
-* asterisk
-% percent
++ plus (addition)
+- minus (subtraction)
+/ slash (divide)
+* asterisk (multiply)
+% percent (percentage of)
 < less-than
 > greater-than
 <= less-than-equal
 >= greater-than-equal
 """
 
+# prints the string specified in the parenthesis
 print("I will now count my chickens:")
 
+# Prints the string "hens" and the calculation of the sum, which is a floating point integer
 print("Hens", 25 + 30 / 6)
+
+#Prints the string "Roosters" and the calculation of the sum, which is a floating point integer
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Prints the string specified in the parenthesis
 print("Now I will count the eggs:")
 
+# Prints the calculation of the sum, which will be a floating point integer
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# Prints the string specified in the parenthesis
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# After calculating the sum, either True or False will be given. In this case it is False, as the answer to 3 + 2 is not less than the answer to 5 - 7
 print(3 + 2 < 5 - 7)
 
+# Prints the string followed by the answer to the sum given, which will be an integer
 print("What is 3 + 2?", 3 + 2)
+
+# Prints the string followed by the answer to the sum given, which will be an integer
 print("What is 5 - 7?", 5 - 7)
 
+# Prints the string specified in the parenthesis
 print("Oh that's why it's False.")
 
+# Prints the string specified in the parenthesis
 print("How about some more.")
 
+# Prints the string followed by True, as 5 is greater than -2
 print("Is it greater?", 5 > -2)
+
+# Prints the string followed by True, as 5 is greater or equal to -2
 print("Is it greater or equal?", 5 >= -2)
+
+# Prints the string followed by False, as 5 is not less than or equal to -2
 print("Is it less or equal?", 5 <= -2)
